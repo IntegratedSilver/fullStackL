86058ce99b68f2269ca009a0814d3065b82c5464
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-04-25 19:52:57 -0700

    first commit

---
+++ api/models/Student.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.models
+{
+    public class Student
+    {
+        public int Id { get; set; }string
+        public string? Name { get; set; }string
+        public string? Address { get; set; }string
+        public string? PhoneNumber { get; set; }string
+        public string? Email { get; set; }
+    }
+}
